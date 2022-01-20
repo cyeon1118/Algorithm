8834a62e2de27703ca432d5e09c6013811a2777d
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-01-21 00:00:45 +0900

    BOJ 1205 java

--- BOJ/implementation/src/boj1205.java
+++ BOJ/implementation/src/boj1205.java
@@ -1,33 +1,72 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj1205 {
-    public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int n = scanner.nextInt();
-            int new_score = scanner.nextInt();
-            int p = scanner.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int scoreNum = Integer.parseInt(st.nextToken());
 
-            Integer[] arr = new Integer[n];
-            for (int i = 0 ; i < n ; i++) {
-                arr[i] = scanner.nextInt();
+        if (N == 0) {
+            System.out.println("1");
+        }
+        else {
+            int rank = 1;
+            int cnt = 1;
+            int tRank = 0;
+
+            int[][] rankList = new int[N][2];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0 ; i < N ; i++) {
+                rankList[i][0] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(arr, Collections.reverseOrder());
 
-            if (n == p && new_score <= arr[arr.length-1]) {
-                System.out.print(-1);
+            rankList[0][1] = rank;
+            for (int j = 0 ; j < N-1 ; j++) {
+                if (rankList[j][0] == rankList[j+1][0]) {
+                    rankList[j+1][1] = rankList[j][1];
+                    cnt++;
+                }
+                else if (rankList[j][0] > rankList[j+1][0]) {
+                    rank = rankList[j+1][1] = rank + cnt;
+                    cnt = 1;
+                }
+            }
+
+            for (int k = 0 ; k < N ; k++) {
+                if (rankList[k][0] == T) {
+                    tRank = rankList[k][1];
+                    break;
+                }
+                else if (rankList[k][0] < T) {
+                    tRank = rankList[k][1];
+                    break;
+                }
+            }
+
+            if (tRank == 0) {
+                tRank = N + 1;
+            }
+
+            if (rankList.length == scoreNum) {
+                if (rankList[scoreNum-1][0] == T) {
+                    System.out.println(-1);
+                }
+                else if (tRank > scoreNum) {
+                    System.out.println(-1);
+                }
+                else {
+                    System.out.println(tRank);
+                }
             }
             else {
-                int rank = 1;
-                for (int i = 0 ; i < arr.length ; i++) {
-                    if(new_score < arr[i])
-                        rank++;
-                    else
-                        break;
-                }
-                System.out.print(rank);
+                System.out.println(tRank);
             }
         }
+    }
 }
