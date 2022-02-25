6345bfbbda9c990a2ba7096fb9549c48026f2e62
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-02-25 23:58:53 +0900

    BOJ 1436 java

--- BOJ/brute force/src/boj1436.java
+++ BOJ/brute force/src/boj1436.java
@@ -1,29 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class boj1436 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        HashMap<Integer, String> num = new HashMap<>(10001);
         int N = Integer.parseInt(br.readLine());
 
-        int key = 0;
-        int n = 666;
-        String str;
+        int n = 0;
+        int num = 665;
 
         do {
-            str = n + "";
-
-            if (str.contains("666")) {
-                num.put(key++, str);
+            if (String.valueOf(++num).contains("666")) {
+                n++;
             }
+        } while (n != N);
 
-            n++;
-        } while (key != 10000);
-
-        System.out.println(num.get(N - 1));
+        System.out.println(num);
     }
 }
