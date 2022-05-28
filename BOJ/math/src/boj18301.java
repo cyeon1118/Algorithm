8a281bc9d43e7b15d48df4dbde9eb6c934566373
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-05-28 20:35:27 +0900

    BOJ 18301 java

---
+++ BOJ/math/src/boj18301.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj18301 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n12 = Integer.parseInt(st.nextToken());
+
+        System.out.println((n1 + 1) * (n2 + 1) / (n12 + 1) - 1);
+    }
+}
