6b7594bf471ea55b0350120be91a6003ef0749ed
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-06-02 18:14:22 +0900

    BOJ 24900 java

--- BOJ/implementation/src/boj24900.java
+++ BOJ/implementation/src/boj24900.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class boj24900 {
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         System.out.println("                                                           :8DDDDDDDDDDDDDD$.                                           \n" +
                 "                                                      DDDNNN8~~~~~~~~~~=~7DNNDNDDDNNI                                   \n" +
