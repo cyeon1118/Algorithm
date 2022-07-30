7b3fc442fa27d86e4917a30546918615172bc321
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-07-30 23:57:33 +0900

    BOJ 1264 java

--- BOJ/string/src/boj1264.java
+++ BOJ/string/src/boj1264.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class boj1264 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         while (true) {
