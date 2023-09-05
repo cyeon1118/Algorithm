1ae0012f7190a6714c853cd891eeb627f539ae49
chaeyeon <cyeon1118@naver.com>
2023-09-05 23:25:56 +0900

    BOJ 25304 java

---
+++ BOJ/math/src/boj25304.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj25304 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int X = Integer.parseInt(br.readLine());
+
+		int N = Integer.parseInt(br.readLine());
+
+		int total = 0;
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int price = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+
+			total += price * n;
+		}
+
+		if (X == total) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+}
